9f38644112d2b62551b55a3ea43e7c2837db2969
Reed Keyes <rbkeyes@gmail.com>
2023-01-24 13:13:05 -0800

    get /api/notes & put /api/notes now working properly. Active note displays on RH side. changed noteId to id and that fixed the issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 
 let notesDb = require('./db/db.json');
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 3002;
 // express app
 const app = express();
 
-// serve static files from public folder
 app.use(express.static('public'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 // GET route for notes html page
@@ -24,37 +24,38 @@ app.get('/notes', (req, res) => {
 
 // get notes api
 app.get('/api/notes', (req, res) => {
-    res.json(notesDb);
-    console.info(notesDb);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // parse data from db.json
+            res.json((JSON.parse(data)))
+        };
+    });
 });
 
 app.post('/api/notes', (req, res) => {
-    // Destructuring assignment for the items in req.body
+    //     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
 
     if (title && text) {
         const newNote = {
             title,
             text,
-            noteId: uuidv4()
+            id: uuidv4()
         };
-
-        // adds newNote to notesDb object array (is displayed upon GET request without needing to restart server) 
-        notesDb.push(newNote);
-        console.log(notesDb);
-
-        // // read existing db.json file
-        // fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        //     if (err) {
-        //         console.error(err);
-        //     } else {
-        //         // parse data from db.json
-        //         const parsedNotes = JSON.parse(data);
-        //         // push newNote to parsedNotes
-        //         parsedNotes.push(newNote);
+        // read existing db.json file
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // parse data from db.json
+                const parsedNotes = JSON.parse(data);
+                // push newNote to parsedNotes
+                parsedNotes.push(newNote);
 
                 // write db.json file with parsedNotes array (includes newNote)
-                fs.writeFile('./db/db.json', JSON.stringify(notesDb, null, 4), (err) => {
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (err) => {
                     if (err) {
                         console.error(err)
                     } else {
@@ -62,8 +63,8 @@ app.post('/api/notes', (req, res) => {
                         console.info('Notes database updated')
                     };
                 });
-            // };
-        // });
+            };
+        });
     } else {
         // if unsuccessful
         res.json('Unable to add note at this time.')
@@ -71,22 +72,20 @@ app.post('/api/notes', (req, res) => {
     };
 });
 
-// app.delete('/api/notes/:noteId', (req, res) => {
-//     const currentId = req.body.noteId
+app.delete('/api/notes/:id', (req, res) => {
+    const currentId = req.body.id
 
-//     const updatedNotes = notesDb.filter(note => note.noteId !== currentId)
-    
-//     notesDb = updatedNotes;
+    const updatedNotes = notesDb.filter(note => note.id !== currentId)
 
-//     fs.writeFile('./db/db.json', JSON.stringify(updatedNotes, null, 4), (err) => {
-//         if (err) {
-//             console.error(err)
-//         } else {
-//             res.json(`Note with id of ${currentId} was successfully deleted`)
-//             console.info(`Note with id of ${currentId} was successfully deleted`)
-//         };
-//     });
-// });
+    fs.writeFile('./db/db.json', JSON.stringify(updatedNotes, null, 4), (err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            res.json(`Note with id of ${currentId} was successfully deleted`)
+            console.info(`Note with id of ${currentId} was successfully deleted`)
+        };
+    });
+});
 
 // route directs back to public index if no match to specified route
 app.get('*', (req, res) =>
