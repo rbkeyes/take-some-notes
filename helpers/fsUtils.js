79538cf4e339f71c3a7d3dd809624f3528d98956
Reed Keyes <rbkeyes@gmail.com>
2023-01-25 11:31:52 -0800

    added fsUtils file to house readFromFile, writeToFile, and readAndAppend functions. exported functions and imported to server.js. GET, PUT, and DELETE are all working in both insomnia and browser

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const util = require('util');
 
-// Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
+const writeToFile = (destination, content) => {
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.info(`\nData written to ${destination}`)
+        }
+    });
+};
 
 const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNote = JSON.parse(data);
+            parsedNote.push(content);
+            writeToFile(file, parsedNote);
+        }
+    });
 };
 
 module.exports = { readFromFile, writeToFile, readAndAppend };
